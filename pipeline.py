ffdb28ca8da0dd179efc59154f7b59d628838f88
tuanpmdev56 <tuanpmdev156@gmail.com>
2026-08-23 11:37:05 +0700

    pipeline

---
+++ pipeline.py
@@ -0,0 +1,11 @@
+import sys
+import pandas as pd
+
+print("arguments", sys.argv)
+
+print("hello pipeline")
+
+df = pd.DataFrame({"A": [1, 2], "B": [3, 4]})
+print(df.head())
+
+df.to_parquet(f"output_day_{sys.argv[1]}.parquet")
